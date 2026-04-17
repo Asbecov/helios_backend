0b4f1cf7fbccd95a165234edcb04b927ee2c6943
Asbecov <m2311505@edu.misis.ru>
2026-04-17 19:49:45 +0300

    feature: changed Marzban Service api provider

--- helios_backend/services/marzban/service.py
+++ helios_backend/services/marzban/service.py
@@ -45,7 +45,7 @@ class MarzbanService:
     """Client for Marzban API operations via marzban public API."""
 
     _DEFAULT_PROXY_NAME = "vless"
-    _DEFAULT_INBOUND_NAME = "vless reality"
+    _DEFAULT_INBOUND_NAME = "vless tls"
     _DEFAULT_PROXY_SETTINGS = ProxySettings(flow="xtls-rprx-vision")
 
     def __init__(self) -> None:
