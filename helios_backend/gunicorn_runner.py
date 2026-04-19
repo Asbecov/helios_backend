0bce303050edb875c3ea550a4fa5ab89d8db821e
Asbecov <m2311505@edu.misis.ru>
2026-04-19 17:16:27 +0300

    fix: fix uvicorn workers

--- helios_backend/gunicorn_runner.py
+++ helios_backend/gunicorn_runner.py
@@ -24,7 +24,7 @@ class UvicornWorker(BaseUvicornWorker):
         "http": "httptools",
         "lifespan": "on",
         "factory": True,
-        "proxy_headers": False,
+        "proxy_headers": True,
     }
 
 
